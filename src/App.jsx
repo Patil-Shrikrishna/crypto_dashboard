b7917d7074425c75d9cae698b656c86290dd4b00
akhilesh <akhilesh>
2023-06-21 18:58:33 +0530

    implimented button, CardTittle, marketcapcard

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import React,{useState} from 'react';
+import ButtonComp from './components/ButtonComp';
+import MarketCapCards from './components/MarketCapCards';
+import CardTittle from './components/CardTittle';
+import {BsFillCaretUpFill } from "react-icons/bs";
+
 function App() {
+
   return (
     <main>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <ButtonComp name="Exchange" />
+      <CardTittle Heading="Cryptocurrency by market cap"/>
+      <MarketCapCards icon = {<BsFillCaretUpFill/>} cryptoName='Bitcoin' MarketCap='$196,235' changePercent={'2.14'}/>
+      
     </main>
   );
 }
---
+++ src/components/CardTittle.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CardTittle = (props) => {
+  return (
+    <div>
+      <h1 className="text-3xl">{props.Heading}</h1>
+    </div>
+  )
+}
+
+export default CardTittle
